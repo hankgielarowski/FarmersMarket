275bc0500de4a35caf9116b5d4985319ac45c465
drewmunnerlyn <drewmunnerlyn@gmail.com>
2016-04-06 09:37:02 -0400

    initial import to master github

---
+++ src/main/java/com/theironyard/FarmersMarketApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmersMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmersMarketApplication.class, args);
+	}
+}
---
+++ src/main/java/com/theironyard/controllers/FarmersMarketController.java
@@ -0,0 +1,28 @@
+package com.theironyard.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.sql.SQLException;
+
+
+/**
+ * Created by drewmunnerlyn on 4/6/16.
+ */
+
+@RestController
+public class FarmersMarketController {
+//    Server dbui;
+//
+//    @PostConstruct
+//    public void construct() throws SQLException, SQLException {
+//        dbui = Server.createWebServer().start();
+//    }
+//
+//    @PreDestroy
+//    public void destroy() {
+//        dbui.stop();
+//    }
+
+}
