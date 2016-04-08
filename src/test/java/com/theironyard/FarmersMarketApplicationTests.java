059bfba2247f5546d1cef160b6a9cbd65e89ddaa
drewmunnerlyn <drewmunnerlyn@gmail.com>
2016-04-08 11:21:43 -0400

    7th import from backend. 1. fixed login route 2. test for login route is working

--- src/test/java/com/theironyard/FarmersMarketApplicationTests.java
+++ src/test/java/com/theironyard/FarmersMarketApplicationTests.java
@@ -1,99 +1,102 @@
-//package com.theironyard;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.theironyard.entities.User;
-//import com.theironyard.services.UserRepository;
-//import com.theironyard.utilities.PasswordStorage;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.FixMethodOrder;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.junit.runners.MethodSorters;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = FarmersMarketApplication.class)
-//@WebAppConfiguration
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-//public class FarmersMarketApplicationTests {
-//
-//    @Autowired
-//    UserRepository users;
-//
-//    @Autowired
-//    WebApplicationContext wap;
-//
-//    MockMvc mockMvc;
-//
-//    @Before
-//    public void before() {
-//		users.deleteAll();
-//        mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
-//    }
-//
+package com.theironyard;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.theironyard.entities.User;
+import com.theironyard.services.UserRepository;
+import com.theironyard.utilities.PasswordStorage;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.http.HttpSession;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FarmersMarketApplication.class)
+@WebAppConfiguration
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class FarmersMarketApplicationTests {
+
+    @Autowired
+    UserRepository users;
+
+    @Autowired
+    WebApplicationContext wap;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void before() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
+    }
+
+    @Test
+    public void test1CreateUser() throws Exception {
+        User user = new User("Alice", "password", "Farmer", "Limehouse Produce", "charleston", "8888", "alice@alice", "password");
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/users")
+                        .content(json)
+                        .contentType("application/json")
+        );
+        Assert.assertTrue(users.count() == 2);
+    }
+
+    @Test
+    public void test2UpdateUser() throws Exception {
+        User user = users.findOne(2);
+        user.setCompanyName("NOT Limehouse Produce 2.0");
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/users/2")
+                        .content(json)
+                        .contentType("application/json")
+                        .sessionAttr("userName", "Alice")
+        );
+        Assert.assertTrue(users.findOne(2).getCompanyName().equals("NOT Limehouse Produce 2.0"));
+    }
+
+    @Test
+    public void test3Login() throws Exception {
+        User user = new User();
+        user.setUserName("Alice");
+        user.setPasswordHash("password");
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(user);
+        ResultActions resAct =
+                mockMvc.perform(
+                MockMvcRequestBuilders.post("/login")
+                        .content(json)
+                        .contentType("application/json")
+        );
+        MvcResult result = resAct.andReturn();
+        MockHttpServletRequest request = result.getRequest();
+        HttpSession session = request.getSession();
+        Assert.assertTrue(session.getAttribute("userName").equals("Alice"));
+    }
+
+
 //    @Test
-//    public void test1AddUser() throws Exception {
-//        User user = new User("Alice", "password", "Farmer", "Limehouse Produce", "charleston", "8888", "alice@alice", "password");
-//
-//        System.out.println();
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        String json = mapper.writeValueAsString(user);
+//    public void test4DeleteUser() throws Exception {
 //        mockMvc.perform(
-//                MockMvcRequestBuilders.post("/users")
-//                        .content(json)
-//                        .contentType("application/json")
-//        );
-//
-//        Assert.assertTrue(users.count() == 1); //should this be one or two since the admin is hardcoded?
-//        //should we be passing the temp variables too? such as validatePassword and userTypeBool?
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    @Test
-//    public void test2DeleteUser() throws Exception {
-////        test1AddUser();
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.delete("/user/1")
-//        );
-//        Assert.assertTrue(users.count() == 0);
-//    }
-//
-//    @Test
-//    public void test3Login() throws Exception {
-//        User user = new User();
-//        user.setUserName("Alice");
-//        user.setPasswordHash("password");
-//        ObjectMapper mapper = new ObjectMapper();
-//        String json = mapper.writeValueAsString(user);
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post("/user")
-//                        .content(json)
-//                        .contentType("application/json")
+//                MockMvcRequestBuilders.delete("/users/2")
 //        );
 //        Assert.assertTrue(users.count() == 1);
 //    }
-//
-//
-//}
+
+}
