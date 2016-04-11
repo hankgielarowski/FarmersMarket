e2b15e368c5a430533016c5fd5a3205408619ec8
hankgielarowski <w_gielarowski@yahoo.com>
2016-04-11 12:02:08 -0400

    new stuffs

--- public/home/home.controller.js
+++ public/home/home.controller.js
@@ -4,9 +4,44 @@ angular
 .module("FarmersMarket")
 .controller("HomeController", HomeController);
 
-HomeController.$inject = ["$scope", "$http", "$location", "$q", "$rootScope", "$routeParams", "HomeService"];
+HomeController.$inject = ["$scope", "$http", "$location", "$q", "$rootScope", "$routeParams", "HomeService",'$uibModal'];
 
-function HomeController($scope,$http,$location,$q,$rootScope,HomeService) {
+function HomeController($scope,$http,$location,$q,$rootScope,$routeParams,HomeService,$uibModal) {
+  $scope.openLogin = function() {
+    console.log("WHAT UP");
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: './auth/views/modallogin.html',
+      controller: 'ModalLoginInstanceCtrl',
+      size: 'lg',
+      resolve: {
+
+      }
+    });
+  }
+
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+
+  $scope.openSignup = function() {
+    console.log("WHAT SIGNUP");
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: './auth/views/modalsignup.html',
+      controller: 'ModalSignupInstanceCtrl',
+      size: 'lg',
+      resolve: {
+
+      }
+    });
+  }
+
+
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
   // HomeService.getUser()
   // .then(function(data) {
   //   console.log("THIS SHOULD BE USERS", data);
