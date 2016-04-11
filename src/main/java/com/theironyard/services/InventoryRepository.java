a65013b20deeb31db5a40a357fc433cbe5994089
MarshallRich <rlmarsh@g.clemson.edu>
2016-04-11 13:44:30 -0400

    adjusted some of the inventory methods and added searching by category route for inventory

--- src/main/java/com/theironyard/services/InventoryRepository.java
+++ src/main/java/com/theironyard/services/InventoryRepository.java
@@ -3,8 +3,11 @@ package com.theironyard.services;
 import com.theironyard.entities.Inventory;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 /**
  * Created by drewmunnerlyn on 4/8/16.
  */
 public interface InventoryRepository extends CrudRepository<Inventory, Integer> {
+    ArrayList<Inventory> findByCategory(String category);
 }
